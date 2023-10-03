8a58ef8ea72821e524f5d3670e5b4e7bd5a1d178
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-10-03 04:41:17 +0200

    triangles

--- src/scop.cpp
+++ src/scop.cpp
@@ -14,16 +14,39 @@ int main(int argc, char** argv) {
 	const std::vector<Vertex>& vertices = object.getVertices();
 	const std::vector<std::vector<int>>& faces = object.getFaces();
 
-	// if (!glfwInit()) {
-	// 	std::cerr << "Failed to initialize GLFW\n";
-	// 	return -1;
-	// }
-	// GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", NULL, NULL);
-	// while (!glfwWindowShouldClose(window)) {
-	// }
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
 
-	// glfwDestroyWindow(window);
-	// glfwTerminate();
+	GLFWwindow* window = glfwCreateWindow(1600, 1200, object.getName().c_str(), NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Set up OpenGL context
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	while (!glfwWindowShouldClose(window)) {
+		// Clear the buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Draw the triangles
+		glBegin(GL_TRIANGLES);
+		for (const auto& face : faces) {
+			for (int i = 0; i < 3; ++i) {
+				const auto& vertex = vertices[face[i]];
+				glVertex3f(vertex.x, vertex.y, vertex.z);
+			}
+		}
+		glEnd();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
 	return 0;
 }
 
