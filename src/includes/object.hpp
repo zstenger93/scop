f7861a462279ab8a1ef79f979870bd400c0af8dd
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-10-03 02:58:14 +0200

    input read

--- src/includes/object.hpp
+++ src/includes/object.hpp
@@ -18,6 +18,7 @@ class Object {
 	std::vector<Vertex> vertices;
 	std::vector<std::vector<int>> faces;
 
+
 	// mtllib attributes
 	float Ns, Ni, d, illum;	 // Specular Exponent, Optical Density, Dissolve, Illumination Model
 	Color Ka;				 // Ambient color
@@ -26,42 +27,18 @@ class Object {
 
    public:
 	// input reading
-	void loadFromObjFile(const std::string& filePath);
 	void loadMtllibFile(const std::string& filePath);
+	void loadFromObjFile(const std::string& filePath);
 
 	// getters
+	const std::string& getName() const { return name; }
 	const std::vector<Vertex>& getVertices() const { return vertices; }
 	const std::vector<std::vector<int>>& getFaces() const { return faces; }
-	const std::string& getName() const { return name; }
 
 	// testing stuff with printing
-	void printVertices() const {
-		std::cout << "Vertices:\n";
-		for (const auto& vertex : vertices) {
-			std::cout << vertex.x << " " << vertex.y << " " << vertex.z << std::endl;
-		}
-	}
-
-	void printFaces() const {
-		std::cout << "Faces:\n";
-		for (const auto& face : faces) {
-			for (const auto& index : face) {
-				std::cout << index << " ";
-			}
-			std::cout << std::endl;
-		}
-	}
-
-	void printMaterialProperties() const {
-		std::cout << "Material Properties:" << std::endl;
-		std::cout << "Ns: " << Ns << std::endl;
-		std::cout << "Ni: " << Ni << std::endl;
-		std::cout << "d: " << d << std::endl;
-		std::cout << "illum: " << illum << std::endl;
-		std::cout << "Ka: " << Ka.r << " " << Ka.g << " " << Ka.b << std::endl;
-		std::cout << "Kd: " << Kd.r << " " << Kd.g << " " << Kd.b << std::endl;
-		std::cout << "Ks: " << Ks.r << " " << Ks.g << " " << Ks.b << std::endl;
-	}
+	void printFaces() const;
+	void printVertices() const;
+	void printMaterialProperties() const;
 };
 
 #endif
--- src/scop.cpp
+++ src/scop.cpp
@@ -1,43 +1,58 @@
 #include "includes/headers.hpp"
 #include "includes/object.hpp"
 
-
-int main(int argc, char **argv) {
-
+int main(int argc, char** argv) {
 	Object object;
 
 	object.loadFromObjFile(argv[1]);
 
+	std::cout << object.getName() << std::endl << std::endl;
+	object.printVertices();
+	object.printFaces();
+	object.printMaterialProperties();
 
-	// object.printVertices();
-	// object.printFaces();
-	// std::cout << object.getName() << std::endl;
-	// object.printMaterialProperties();
+	const std::vector<Vertex>& vertices = object.getVertices();
+	const std::vector<std::vector<int>>& faces = object.getFaces();
 
-    // if (!glfwInit()) {
-    //     std::cerr << "Failed to initialize GLFW\n";
-    //     return -1;
-    // }
-    // // Create a windowed mode window and its OpenGL context
-    // GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", NULL, NULL);
-    // if (!window) {
-    //     std::cerr << "Failed to create GLFW window\n";
-    //     glfwTerminate();
-    //     return -1;
-    // }
-    // // Make the window's context current
-    // glfwMakeContextCurrent(window);
-    // while (!glfwWindowShouldClose(window)) {
-    //     // Render graphics here
+	// if (!glfwInit()) {
+	// 	std::cerr << "Failed to initialize GLFW\n";
+	// 	return -1;
+	// }
+	// GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", NULL, NULL);
+	// while (!glfwWindowShouldClose(window)) {
+	// }
 
-    //     // Swap front and back buffers
-    //     // glfwSwapBuffers(window);
-
-    //     // Poll for and process events
-    //     glfwPollEvents();
-    // }
-    // glfwDestroyWindow(window);
-    // glfwTerminate();
-    // return 0;
+	// glfwDestroyWindow(window);
+	// glfwTerminate();
+	return 0;
 }
 
+void Object::printVertices() const {
+	std::cout << "Vertices:\n";
+	for (const auto& vertex : vertices) {
+		std::cout << vertex.x << " " << vertex.y << " " << vertex.z << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void Object::printFaces() const {
+	std::cout << "Faces:\n";
+	for (const auto& face : faces) {
+		for (const auto& index : face) {
+			std::cout << index << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void Object::printMaterialProperties() const {
+	std::cout << "Material Properties:" << std::endl;
+	std::cout << "Ns: " << Ns << std::endl;
+	std::cout << "Ka: " << Ka.r << " " << Ka.g << " " << Ka.b << std::endl;
+	std::cout << "Kd: " << Kd.r << " " << Kd.g << " " << Kd.b << std::endl;
+	std::cout << "Ks: " << Ks.r << " " << Ks.g << " " << Ks.b << std::endl;
+	std::cout << "Ni: " << Ni << std::endl;
+	std::cout << "d: " << d << std::endl;
+	std::cout << "illum: " << illum << std::endl;
+}
