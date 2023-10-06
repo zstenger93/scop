f21f20e966b88c02f131b40413141b248848c1b7
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-10-06 13:54:51 +0200

    uv stuff trying

--- src/new/scop.cpp
+++ src/new/scop.cpp
@@ -9,23 +9,27 @@ const char *vertexShaderSource = R"glsl(
 	#version 330 core
 
 	layout (location = 0) in vec3 aPos;
+	layout (location = 1) in vec3 aColor;
+	out vec3 ourColor;
 
 	void main() {
-		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		gl_Position = vec4(aPos, 1.0);
+		ourColor = aColor;
 	}
 	)glsl";
 
 const char *fragmentShaderSource = R"glsl(
 	#version 330 core
 
+	in vec3 ourColor;
 	out vec4 FragColor;
 
 	void main() {
-		FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+		FragColor = vec4(ourColor, 1.0f);
 	}
 	)glsl";
 
-int main() {
+int main(int argc, char **argv) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -86,25 +90,31 @@ int main() {
 
 	// VETICES
 
-	float vertices[] = {0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f};
+	float vertices[] = {
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+		};
 
 	// INDICES
 
-	unsigned int indices[] = {0, 1, 3, 1, 2, 3};
+	// unsigned int indices[] = {0, 1, 3, 1, 2, 3};
 
 	// VBO, VAO AND EBO
 
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	// glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// MAIN LOOP FOR RENDERING
 
@@ -115,16 +125,21 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		// int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		// float timeValue = glfwGetTime();
+		// float greenValue = sin(timeValue) / 2.0f +  0.5f;
+		// glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	// glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
