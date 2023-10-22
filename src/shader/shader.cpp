0e64e203543f2a11c97f7d427b116a3b181db615
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-10-22 08:14:16 +0200

    code cleaning

--- src/shader/shader.cpp
+++ src/shader/shader.cpp
@@ -42,39 +42,51 @@ Shader::Shader(const char *vertexPath, const char *fragmentPath) {
 	glDeleteShader(fragment);
 }
 void Shader::use() { glUseProgram(ID); }
+
 void Shader::setBool(const std::string &name, bool value) const {
 	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
+
 void Shader::setInt(const std::string &name, int value) const {
 	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
+
 void Shader::setFloat(const std::string &name, float value) const {
 	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
+
 void Shader::setVec2(const std::string &name, const glm::vec2 &value) const {
 	glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
+
 void Shader::setVec2(const std::string &name, float x, float y) const {
 	glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
 }
+
 void Shader::setVec3(const std::string &name, const glm::vec3 &value) const {
 	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
+
 void Shader::setVec3(const std::string &name, float x, float y, float z) const {
 	glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
 }
+
 void Shader::setVec4(const std::string &name, const glm::vec4 &value) const {
 	glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
+
 void Shader::setVec4(const std::string &name, float x, float y, float z, float w) const {
 	glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
 }
+
 void Shader::setMat2(const std::string &name, const glm::mat2 &mat) const {
 	glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
+
 void Shader::setMat3(const std::string &name, const glm::mat3 &mat) const {
 	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
+
 void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const {
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
@@ -103,7 +115,6 @@ void Shader::setModel(Camera &camera, Shader &shader, glm::mat4 &model) {
 void Shader::settings(RenderMode &renderMode, unsigned int &texture) {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	glPolygonMode(GL_FRONT_AND_BACK, (renderMode == WIREFRAME) ? GL_LINE
 									 : (renderMode == POINTS)  ? GL_POINT
 															   : GL_FILL);
